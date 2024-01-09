d01dcfaadbf84e60afd26188867ca03c891f8bdd
maintheme <oligarh20031@mail.ru>
2024-01-09 22:31:37 +0000

    !

---
+++ configs/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+	alias ls "lsd -1"
+	alias hx "helix"
+	alias lt "ls --tree"
+	alias icat="kitty +kitten icat"
+	set fish_greeting ""
+end
+
+# cat ~/.cache/wal/sequences &
+# source ~/.cache/wal/colors.fish
---
+++ zeze.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USER_DIR=/home/maintheme
+DOTS_DIR=$USER_DIR/dotfiles
+
+foo () {
+  rm -rf $USER_DIR/.config/$1
+  ln -s $DOTS_DIR/configs/$1 $USER_DIR/.config
+}
+
+foo "rofi"
+foo "kitty"
+foo "fish"
+foo "helix"
+foo "awesome"
+foo "neofetch"
+foo "sddm"
+
+# if [ -d $USER_DIR/.config/Code/User/ ]; then
+#   ln -sf $DOTS_DIR/configs/vscode/settings.json $USER_DIR/.config/Code/User/
+# fi
+
+ln -sf $DOTS_DIR/x11/user-dirs.dirs $USER_DIR/.config/
+ln -sf $DOTS_DIR/x11/.xinitrc $USER_DIR/
+
+cp -f $DOTS_DIR/x11/30-touchpad.conf /etc/X11/xorg.conf.d/
+cp -f $DOTS_DIR/x11/00-keyboard.conf /etc/X11/xorg.conf.d/
+cp -f $DOTS_DIR/x11/vconsole.conf /etc/
