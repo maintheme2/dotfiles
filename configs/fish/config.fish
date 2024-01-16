6c9e49b4266697a7833ee0aaad0259d99bb4c956
maintheme <oligarh20031@mail.ru>
2024-01-16 18:46:33 +0300

    minor

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -2,9 +2,11 @@ if status is-interactive
 	alias ls "lsd -1"
 	alias hx "helix"
 	alias lt "ls --tree"
-	alias icat="kitty +kitten icat"
+	alias icat "kitty +kitten icat"
+	alias bat "cat /sys/class/power_supply/BAT0/capacity"
 	set fish_greeting ""
 end
 
 cat ~/.cache/wal/sequences &
 # source ~/.cache/wal/colors.fish
+neofetch
